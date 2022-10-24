d89b322d5a43933b9ab30143556b17547d697c2b
Thiernope <ntirandth@gmail.com>
2022-10-24 11:54:14 +0200

    adding new resume

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get("/download-pdf", (req, res)=> {
-    res.download("./thierry_cv.pdf")
+    res.download("./Thierry_Ntirandekura_dev_resume..docx.pdf")
 })
 
 const PORT = process.env.PORT || 5000;
